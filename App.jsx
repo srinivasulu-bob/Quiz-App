7a70202ba2e512507736484a0a04343fe6f1b63f
srinivasulu <srinivasulupunamalli5@gmail.com>
2025-09-04 21:43:05 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import './App.css'
+import Quiz from './Components/Quiz/Quiz'
+
+const App = () => {
+  return (
+    <div>
+          <Quiz/>
+    </div>
+  )
+}
+
+export default App
